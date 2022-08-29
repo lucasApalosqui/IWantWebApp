21c68f8496b609d0a5653a2a7aa828b0f08c43fa
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-08-29 09:55:04 -0300

    @lucasApalosqui [ Creating Product Domain ]

--- IWantApp/src/Domain/Products/Product.cs
+++ IWantApp/src/Domain/Products/Product.cs
@@ -1,12 +1,14 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Category
+public class Product
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+    public bool HasStock { get; set; }
     public string CreateBy { get; set; }
     public DateTime CreateOn { get; set; }
     public string EditedBy { get; set; }
     public DateTime EditedOn { get; set; }
 }
-
