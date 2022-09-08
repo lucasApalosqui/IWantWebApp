a88f0c3ea40ccbd819131808a7d272c97717d5eb
LucasApalosqui <lucas.apalosqui@gmail.com>
2022-09-08 13:54:15 -0300

    @lucasApalosqui [ Endpoint Get products wuithout authorization ]

---
+++ IWantApp/src/Endpoints/Products/ProductGetShowcases.cs
@@ -0,0 +1,23 @@
+﻿using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace IWantApp.Endpoints.Products;
+
+public class ProductGetShowCases
+{
+    public static string Template => "/products/showcase";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    [AllowAnonymous]
+    public static async Task<IResult> Action(ApplicationDbContext context)
+    {
+        var products = context.Products.Include(p => p.Category)
+            .Where(p => p.HasStock && p.Category.Active)
+            .OrderBy(p => p.Name)
+            .ToList();
+        var results = products.Select(p => new ProductResponse(p.Name, p.Category.Name, p.Description, p.HasStock, p.Active, p.Price));
+        return Results.Ok(results);
+    }
+}
--- IWantApp/src/Program.cs
+++ IWantApp/src/Program.cs
@@ -81,6 +81,7 @@ app.MapMethods(EmployeeGetAll.Template, EmployeeGetAll.Methods, EmployeeGetAll.H
 app.MapMethods(TokenPost.Template, TokenPost.Methods, TokenPost.Handle);
 app.MapMethods(ProductGetAll.Template, ProductGetAll.Methods, ProductGetAll.Handle);
 app.MapMethods(ProductPost.Template, ProductPost.Methods, ProductPost.Handle);
+app.MapMethods(ProductGetShowCases.Template, ProductGetShowCases.Methods, ProductGetShowCases.Handle);
 
 app.UseExceptionHandler("/error");
 app.Map("/error", (HttpContext http) =>
